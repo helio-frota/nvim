432d14039fa1c498988c7f54bfb8c14f78e29645
Helio Frota <00hf11@gmail.com>
2025-01-21 18:21:55 -0300

    21 loaded plugins on startup now

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system {
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    }
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -17,10 +17,10 @@ vim.cmd [[colorscheme tp]]
 vim.opt.conceallevel = 1
 
 require("lazy").setup {
-    spec = {
-        { import = "plugins" },
-    },
-    checker = { enabled = true },
+  spec = {
+    { import = "plugins" },
+  },
+  checker = { enabled = true },
 }
 
 require "core.options"
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,20 +1,30 @@
 return {
   "echasnovski/mini.nvim",
   config = function()
-    require("mini.cursorword").setup()
-    require("mini.clue").setup()
     require("mini.align").setup()
+    require("mini.clue").setup()
     require("mini.comment").setup()
+    require("mini.cursorword").setup()
+    require("mini.files").setup()
+    require("mini.hipatterns").setup()
+    require("mini.notify").setup()
     require("mini.pairs").setup()
     require("mini.starter").setup()
     require("mini.tabline").setup()
-    require("mini.files").setup()
     require("mini.trailspace").setup()
-    require("mini.notify").setup()
     local statusline = require "mini.statusline"
     statusline.setup { use_icons = true }
     statusline.section_location = function()
       return "%2l:%-2v"
     end
+    local hipatterns = require "mini.hipatterns"
+    hipatterns.setup {
+      highlighters = {
+        fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+        hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+        todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+        note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+      },
+    }
   end,
 }
